c986f0ed7bae91972dd3306c6a63c01f3153b7c7
anushkah-01 <anushkahirloskar@gmail.com>
2025-06-29 08:16:21 -0700

    Add missing Editor component

--- components/Editor.js
+++ components/Editor.js
@@ -4,7 +4,11 @@ import 'react-quill/dist/quill.snow.css';
 const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
 const Editor = ({ value, onChange }) => {
-  return <ReactQuill theme="snow" value={value} onChange={onChange} />;
+  return (
+    <div>
+      <ReactQuill theme="snow" value={value} onChange={onChange} />
+    </div>
+  );
 };
 
 export default Editor;
